2a4153542466e59fcbd53e855c15845096a02a7d
PhongNgh <duchieutran1302@gmail.com>
2025-02-14 09:34:41 +0700

    add config Cloudinary

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from flask_pymongo import PyMongo
 from flask_jwt_extended import JWTManager
 from flask_caching import Cache
 from config import Config
+import cloudinary
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,6 +11,12 @@ app.config.from_object(Config)
 mongo = PyMongo(app)
 jwt = JWTManager(app)
 cache = Cache(app)
+
+cloudinary.config(
+    cloud_name=app.config["CLOUDINARY_CLOUD_NAME"],
+    api_key=app.config["CLOUDINARY_API_KEY"],
+    api_secret=app.config["CLOUDINARY_API_SECRET"]
+)
 
 from routes import *  # Import routes sau khi khởi tạo mongo
 
