d4a55e8c491c3ffd38ba425fdf3cd2fd110e41e7
PhongNgh <duchieutran1302@gmail.com>
2025-02-14 09:37:19 +0700

    prefix #1

--- models.py
+++ models.py
@@ -24,3 +24,36 @@ class User:
     def validate_password(cls, stored_password, provided_password):
         """Kiểm tra mật khẩu"""
         return check_password_hash(stored_password, provided_password)
+
+class Product:
+    def __init__(self, name, price, quantity, description, image_url):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.description = description
+        self.image_url = image_url
+
+    def save(self):
+        mongo.db.products.insert_one({
+            "name": self.name,
+            "price": self.price,
+            "quantity": self.quantity,
+            "description": self.description,
+            "image": self.image_url
+        })
+
+    @classmethod
+    def find_by_name(cls, name):
+        return mongo.db.products.find_one({"name": name})
+
+    @classmethod
+    def find_by_id(cls, product_id):
+        return mongo.db.products.find_one({"_id": product_id})
+
+    @classmethod
+    def update(cls, product_id, updates):
+        mongo.db.products.update_one({"_id": product_id}, {"$set": updates})
+
+    @classmethod
+    def delete(cls, product_id):
+        mongo.db.products.delete_one({"_id": product_id})
